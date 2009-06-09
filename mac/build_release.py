70e2d6c9ad8720976e67951cc7c568021edaec17
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-06-09 15:05:56 -0700

    started on the build script

---
+++ mac/build_release.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# xcodebuild -project OpenDNSDiagnostic.xcodeproj -configuration Release clean
+# xcodebuild -project OpenDNSDiagnostic.xcodeproj -configuration Release build
+# zip -9 -r OpenDNSDiagnostic-0.1.zip OpenDNSDiagnostic.app
+
+def main():
+	
+if __name__ == "__main__":
+	main()
